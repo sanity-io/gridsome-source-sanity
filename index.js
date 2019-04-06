54c6b3fdfe203c55c2a37613dd43be88f901e437
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-04-06 14:38:31 +0300

    Attempt at defining all types + fields ahead of time

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const {
-  startCase
-} = require('lodash')
-const {
-  valueFromASTUntyped
-} = require('graphql')
+const {startCase} = require('lodash')
+const {valueFromASTUntyped} = require('graphql')
 const axios = require('axios')
 const pumpIt = require('pump')
 const split = require('split2')
@@ -11,6 +7,9 @@ const through = require('through2')
 const sanityClient = require('@sanity/client')
 const addFieldPrefixes = require('./addFieldPrefixes')
 const getRemoteGraphQLSchema = require('./remoteGraphQLSchema')
+
+const basicTypes = ['String', 'Int', 'Float', 'Boolean', 'ID']
+
 class SanitySource {
   static defaultOptions() {
     return {
@@ -30,12 +29,7 @@ class SanitySource {
     this.store = api.store
     this.typesIndex = {}
 
-    const {
-      projectId,
-      dataset,
-      token,
-      useCdn
-    } = options
+    const {projectId, dataset, token, useCdn} = options
 
     this.client = sanityClient({
       projectId,
@@ -46,43 +40,69 @@ class SanitySource {
 
     api.loadSource(async store => {
       this.graphqlTypes = await getRemoteGraphQLSchema(this.client)
+      await this.prepareContentTypes(store)
       await this.getDocuments(store)
     })
   }
 
-  prepareContentType(store, typeName, docExample) {
-    const contentType = store.addContentType({
-      typeName,
-    })
+  prepareContentTypes(store) {
+    this.graphqlTypes.forEach(type => this.prepareContentType(type, store))
+  }
 
-    const graphqlName = getTypeName(docExample._type)
-    const graphqlType = this.graphqlTypes.find(item => item.name.value === graphqlName)
-    if (!graphqlType) {
-      console.warn(`Could not find GraphQL type for type ${typeName}`)
-      return
-    }
+  // eslint-disable-next-line class-methods-use-this
+  prepareContentType(graphqlType, store) {
+    const graphqlName = graphqlType.name.value
+    const typeName = normalizeTypeName(store.makeTypeName(graphqlName))
+    const contentType = store.addContentType({typeName})
 
-    graphqlType.fields.map(getJsonAliasDirective).filter(Boolean).forEach(jsonField => {
-      contentType.addSchemaField(`_raw${ucFirst(jsonField.aliasFor)}`, ({
-        graphql
-      }) => ({
-        type: graphql.GraphQLJSON,
-        resolve: doc => addFieldPrefixes(doc.fields[jsonField.aliasFor])
-      }))
-    })
+    // Add JSON aliases
+    graphqlType.fields
+      .map(getJsonAliasDirective)
+      .filter(Boolean)
+      .forEach(jsonField =>
+        contentType.addSchemaField(`_raw${ucFirst(jsonField.aliasFor)}`, ({graphql}) => ({
+          type: graphql.GraphQLJSON,
+          resolve: doc => addFieldPrefixes(doc.fields[jsonField.aliasFor])
+        }))
+      )
+
+    // Add regular, non-primitive fields
+    graphqlType.fields
+      .filter(field => !getJsonAliasDirective(field))
+      .map(field => ({...field, unwrappedType: unwrapType(field.type)}))
+      .forEach(field => {
+        const unwrappedName = field.unwrappedType.name.value
+        const unwrapped = makeNullable(field)
+        const isList = unwrapped.kind === 'ListType'
+
+        if (basicTypes.includes(unwrappedName)) {
+          return contentType.addSchemaField(field.name.value, ({graphql}) => ({
+            type: isList
+              ? graphql.GraphQLList(graphql[`GraphQL${unwrappedName}`])
+              : graphql[`GraphQL${unwrappedName}`]
+          }))
+        }
+
+        if (unwrappedName === 'DateTime') {
+          // :murgh: Gridsome doesn't seem to expose the date type?
+          return contentType.addSchemaField(field.name.value, ({graphql}) => ({
+            type: isList ? graphql.GraphQLList(graphql.GraphQLString) : graphql.GraphQLString
+          }))
+        }
+
+        return contentType.addSchemaField(field.name.value, ({graphql, nodeTypes}) => {
+          const name = normalizeTypeName(store.makeTypeName(unwrappedName))
+          const resolve = inp => ({id: 'abc123', alt: 'foo'})
+          return {type: isList ? graphql.GraphQLList(nodeTypes[name]) : nodeTypes[name], resolve}
+        })
+      })
   }
 
   async getDocuments(store) {
-    const {
-      dataset,
-      overlayDrafts,
-      watchMode,
-      token
-    } = this.client.config()
+    const {dataset, overlayDrafts, watchMode, token} = this.client.config()
 
     const url = this.client.getUrl(`/data/export/${dataset}`)
     const inputStream = await getDocumentStream(url, token)
-    const typesSeen = new Set()
 
     await pump([
       inputStream,
@@ -90,34 +110,18 @@ class SanitySource {
       rejectOnApiError(),
       removeSystemDocuments(),
       through.obj((doc, enc, callback) => {
-        const {
-          _id,
-          _createdAt,
-          _updatedAt,
-          _type
-        } = doc
-
-        const typeName = store.makeTypeName(_type)
-        if (!typesSeen.has(typeName)) {
-          this.prepareContentType(store, typeName, doc)
-          typesSeen.add(typeName)
-        }
-
+        const {_id, _createdAt, _updatedAt, _type} = doc
+        const gqlTypeName = getGraphQLTypeName(_type)
+        const typeName = normalizeTypeName(store.makeTypeName(gqlTypeName))
         const collection = store.getContentType(typeName)
-        const fields = {
-          ...doc,
-          date: _createdAt,
-          updatedAt: _updatedAt
-        }
-
         collection.addNode({
           id: _id,
           slug: doc.title,
-          fields
+          fields: {...doc, date: _createdAt, updatedAt: _updatedAt}
         })
 
         callback()
-      }),
+      })
     ])
   }
 }
@@ -126,12 +130,12 @@ function ucFirst(str) {
   return (str[0] || '').toUpperCase() + str.slice(1)
 }
 
-function getTypeName(str) {
+function getGraphQLTypeName(str) {
   return startCase(str).replace(/\s+/g, '')
 }
 
 function rejectOnApiError() {
-  return through.obj(function (sanityDoc, string, callback) {
+  return through.obj(function(sanityDoc, string, callback) {
     const doc = sanityDoc
     if (doc._id && doc._type) {
       callback(null, doc)
@@ -149,7 +153,7 @@ function rejectOnApiError() {
 }
 
 function removeSystemDocuments() {
-  return through.obj(function (doc, string, callback) {
+  return through.obj(function(doc, string, callback) {
     if (doc && doc._id && doc._id.startsWith('_.')) {
       return callback()
     }
@@ -159,12 +163,8 @@ function removeSystemDocuments() {
 }
 
 function getDocumentStream(url, token) {
-  const auth = token ? {
-    Authorization: `Bearer ${token}`
-  } : {}
-  const userAgent = {
-    'User-Agent': 'gridsome-source-sanity'
-  }
+  const auth = token ? {Authorization: `Bearer ${token}`} : {}
+  const userAgent = {'User-Agent': 'gridsome-source-sanity'}
   const headers = {
     ...userAgent,
     ...auth
@@ -185,9 +185,7 @@ function getJsonAliasDirective(field) {
   }
 
   return {
-    aliasFor: valueFromASTUntyped(
-      alias.arguments.find(arg => arg.name.value === 'for').value
-    )
+    aliasFor: valueFromASTUntyped(alias.arguments.find(arg => arg.name.value === 'for').value)
   }
 }
 
@@ -201,6 +199,29 @@ function pump(streams) {
       }
     })
   )
+}
+
+function unwrapType(typeNode) {
+  if (['NonNullType', 'ListType'].includes(typeNode.kind)) {
+    const wrappedType = typeNode
+    return unwrapType(wrappedType.type)
+  }
+
+  return typeNode
+}
+
+function makeNullable(typeNode) {
+  if (typeNode.kind === 'NonNullType') {
+    return makeNullable(typeNode.type)
+  } else if (typeNode.kind === 'ListType') {
+    return {...typeNode, type: makeNullable(typeNode.type)}
+  }
+
+  return typeNode
+}
+
+function normalizeTypeName(typeName) {
+  return typeName.replace(/^SanitySanity/, 'Sanity')
 }
 
 module.exports = SanitySource
